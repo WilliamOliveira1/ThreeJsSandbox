825ff97b72251ef43f32c253c1257982c83c07e9
WilliamOliveira1 <1704639@aluno.univesp.br>
2023-09-10 22:33:14 -0300

    create skybox | create basic reflect material

--- Js/createObjectHelper.js
+++ Js/createObjectHelper.js
@@ -1,14 +1,19 @@
-class Create3dObjectsHelper {
-    constructor(threeJsInstance) {
-        this.THREE = threeJsInstance;
+import * as THREE from 'three';
+export class Create3dObjectsHelper {
+    constructor() {
+        this.THREE = THREE;
         this.materialType = {
-            basicMaterial: this.THREE.MeshBasicMaterial,
-            standardMaterial: this.THREE.MeshStandardMaterial
+            basicMaterial: THREE.MeshBasicMaterial,
+            standardMaterial: THREE.MeshStandardMaterial
         }        
     }
 
+    /**
+     * Get textures
+     * @returns {Object} textures objects
+     */
     textures() {
-        this.textureLoader = new this.THREE.TextureLoader();
+        this.textureLoader = new THREE.TextureLoader();
         return {
             nest: this.textureLoader.load("\\textures\\nest.jpg"),
             stone: this.textureLoader.load("textures\\stones.jpg"),
@@ -35,10 +40,11 @@ class Create3dObjectsHelper {
      * Basic material values (Get and Set)
      * @param {String} color - Hex color Codes - the default value is "#FFFFFF" (white)
      * @param {Boolean} isFogAffected - Whether the material is affected by fog. Default is true.
+     * @param {Object} envMap - The environment map. Default is null.
      * @returns {Object} a standard material object value
      */
-    basicMaterialValues({color = "#FFFFFF", isFogAffected = true} = {}) {
-        return {color, isFogAffected};
+    basicMaterialValues({color = "#FFFFFF", isFogAffected = true, envMap = null} = {}) {
+        return {color, isFogAffected, envMap};
     }
     
     /**
@@ -61,7 +67,7 @@ class Create3dObjectsHelper {
      * @returns {Object} Threejs Mesh object 
      */
     createBasicSphereObject(radius = 1, widhtSegments = 32, heightSegments = 16, materialObject = this.basicMaterialValues()) {        
-        const sphereGeometry = new this.THREE.SphereGeometry(radius, widhtSegments, heightSegments);
+        const sphereGeometry = new THREE.SphereGeometry(radius, widhtSegments, heightSegments);
         return this.create3dObject(sphereGeometry, materialObject);
     }
     
@@ -74,8 +80,24 @@ class Create3dObjectsHelper {
      * @returns {Object} Threejs Mesh object 
      */
     createBasicCubeObject(width = 1, height = 1, depth = 1, materialObject = this.basicMaterialValues()) {
-        const geometryCube = new this.THREE.BoxGeometry(width, height, depth);
+        const geometryCube = new THREE.BoxGeometry(width, height, depth);
         return this.create3dObject(geometryCube, materialObject);
+    }
+
+    /**
+     * Create basic cube with edges 3d object
+     * @param {Number} width — The length of the edges parallel to the X axis. Optional; default value is 1.
+     * @param {Number} height — The length of the edges parallel to the Y axis. Optional; default value is 1.
+     * @param {Number} depth — The length of the edges parallel to the Z axis. Optional; default value is 1.
+     * @param {Object} materialObject - material of the 3d object, default value is basicMaterialValues object
+     * @returns {Object} Threejs Mesh object 
+     */
+    createBasicCubeWithEdgesObject(width = 1, height = 1, depth = 1, edgesColor = "", materialObject = this.basicMaterialValues()) {
+        const geometryCube = new THREE.BoxGeometry(width, height, depth);
+        const edges = new THREE.EdgesGeometry(geometryCube);
+        const line = new THREE.LineSegments(edges, new this.THREE.LineBasicMaterial({ color: edgesColor }));
+        const cube = this.create3dObject(geometryCube, materialObject)
+        return {object: cube, edges: line};
     }
 
     /**
@@ -88,8 +110,35 @@ class Create3dObjectsHelper {
      * @returns {Object} Threejs Mesh object 
      */
     createBasicCylinderObject(radiusTop = 1, radiusBottom = 1, height = 1, radialSegments = 32, materialObject = this.basicMaterialValues()) {
-        const geometryCylinder = new this.THREE.CylinderGeometry(radiusTop, radiusBottom, height, radialSegments);
+        const geometryCylinder = new THREE.CylinderGeometry(radiusTop, radiusBottom, height, radialSegments);
         return this.create3dObject(geometryCylinder, materialObject);
+    }
+
+    /**
+     * Create basic plane 3d object
+     * @param {Number} width — width of the plane. Default value is 30.
+     * @param {Number} height — Height of the plane. Default value is 30.
+     * @param {Object} rotationValueX - Value to rotate the plane in X. Default value is null.
+     * @param {Object} rotationValueY - Value to rotate the plane in Y. Default value is null.
+     * @param {Object} rotationValueZ - Value to rotate the plane in Z. Default value is null.
+     * @param {Object} materialObject - material of the 3d object, default value is basicMaterialValues object
+     * @returns {Object} Threejs Mesh object 
+     */
+    createBasicPlaneObject(width = 30, height = 30, rotationValueX = 0, rotationValueY = 0, rotationValueZ = 0, materialObject = this.basicMaterialValues()) {
+        const  planeGeometry = new THREE.PlaneGeometry(width, height)
+        const plane = this.create3dObject(planeGeometry, materialObject);
+        if(rotationValueX !== 0) {
+            plane.rotation.x = rotationValueX;
+        }
+
+        if(rotationValueY !== 0) {
+            plane.rotation.y = rotationValueY;
+        }
+
+        if(rotationValueZ !== 0) {
+            plane.rotation.z = rotationValueZ;
+        }
+        return plane;
     }
 
     /**
--- Js/script.js
+++ Js/script.js
@@ -1,8 +1,9 @@
 import * as THREE from 'three';
 import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls'
+import {Create3dObjectsHelper} from './createObjectHelper'
 class threeJs {
-    constructor(create3dObjectsHelperInstance) {
-        this.create3dObjectsHelper = create3dObjectsHelperInstance;
+    constructor() {
+        this.create3dObjectsHelper = new Create3dObjectsHelper();
         this.object = null;
         this.scene = null;
         this.camera = null;
@@ -14,45 +15,51 @@ class threeJs {
     /**
      * Init threejs objects
      */
-    async initTheeJs() {
+    async initTheeJs() {        
         this.setSceneAndCam();
-        this.setRenderer();
-        this.loadTextures();
-        this.pointLight = new THREE.DirectionalLight(0xffffff, 0.1);
-        this.pointLight1 = new THREE.DirectionalLight(0xffffff, 0.1);
-        // create 3d objects
-        // sphere
-        this.texturesValues = this.create3dObjectsHelper.textures();
-        this.metallicNestMaterial = this.create3dObjectsHelper.standardMaterialValues({normalMap: this.texturesValues.nest, roughness: 0.8, metalness: 0.3});
-        const sphere = this.create3dObjectsHelper.createBasicSphereObject(1, 64, 64, this.metallicNestMaterial);
-
-        // Cube
-        const cube = this.create3dObjectsHelper.createBasicCubeObject(2,2,2,this.metallicNestMaterial);
-
-        const geometry = new THREE.BoxGeometry(2, 2, 2);
-        const material = new THREE.MeshBasicMaterial({ color: 0x20b2aa });
-        const cube1 = new THREE.Mesh(geometry, material);        
-
-        const edges = new THREE.EdgesGeometry(geometry);
-        this.line = new THREE.LineSegments(edges, new THREE.LineBasicMaterial({ color: 0x000000 }));        
-        this.scene.add(this.line);
-        // cylinder
-        const geometryCylinder = new THREE.CylinderGeometry(2, 2, 7, 50);
-        const cylinder = this.create3dObjectsHelper.create3dObject(geometryCylinder);
-
-        const  planeGeometry = new THREE.PlaneGeometry(30, 30)
-        const plane = this.create3dObjectsHelper.create3dObject(planeGeometry);
-        this.scene.add(plane);
-        plane.rotation.x = -0.5 * Math.PI;
-        // this.changeObjectFromScene(cube);
-        this.changeObjectFromScene(cube);
-        this.moveObject();
+        this.setRenderer("#8bc34a");
+        this.setSkyBox();
+        this.preLoadTextures();
         this.orbit = new OrbitControls(this.camera, this.renderer.domElement);
-        this.gridHelper = new THREE.GridHelper(30, 60)
-        this.dLightHelper = new THREE.DirectionalLightHelper(this.pointLight, 5);
-        this.scene.add(this.gridHelper);
-        this.scene.add(this.dLightHelper);
+        const sphere = this.create3dObjectsHelper.createBasicSphereObject(1, 32, 32, this.sphereReflectMaterial);
+        this.gridHelper = new THREE.GridHelper(30, 60)        
+        this.scene.add(this.gridHelper);       
+        this.changeObjectFromScene(sphere);
+        this.moveObject([this.object]);
         this.renderer.shadowMap.enabled = true;
+    }
+
+    /**
+     * Add multiples objects in scene
+     * @param {Object} objects - ThreeJs objects
+     */
+    addObjectsInScene(objects) {
+        objects.forEach((object) => {
+            this.scene.add(object);
+        });
+    }
+
+    setSkyBox() {
+        let images = [
+            "textures\\Day_Light_skyBox\\Box_Left_1.bmp", "textures\\Day_Light_skyBox\\Box_Right_2.bmp",
+            "textures\\Day_Light_skyBox\\Box_Top_3.bmp", "textures\\Day_Light_skyBox\\Box_Bottom_4.bmp",
+            "textures\\Day_Light_skyBox\\Box_Back_5.bmp", "textures\\Day_Light_skyBox\\Box_Front_6.bmp"
+        ]
+        let loader = new THREE.CubeTextureLoader();
+        this.scene.background = loader.load(images);
+        this.renderer.render(this.scene, this.camera);        
+        this.runScene();
+    }
+
+    /**
+     * Set threejs WebGLRenderer renderer
+     * @param {String} color - HEX color value
+     */
+    setRenderer(color) {
+        this.renderer = new THREE.WebGLRenderer({antialias: true});
+        this.renderer.setSize(window.innerWidth, window.innerHeight);
+        this.renderer.setClearColor(color, 1);
+        document.body.appendChild(this.renderer.domElement);
     }
 
     /**
@@ -63,52 +70,34 @@ class threeJs {
         this.scene = new THREE.Scene();
         // create camera
         this.camera = new THREE.PerspectiveCamera(
-            40,
+            70,
             window.innerWidth/window.innerHeight,
-            0.1,
-            1000
+            1,
+            5000
         );
         //set initial position of camera
-        this.camera.position.set(0, 0, 25);
-        this.axesHelper();
-    }
-
-    axesHelper() {
-        this.axesHelperObj = new THREE.AxesHelper(5);
-        this.scene.add(this.axesHelperObj);        
+        this.camera.position.set(0, 20, 50);
+        this.axesHelper(10);
     }
 
     /**
-     * Set threejs WebGLRenderer renderer
+     * Set AxesHelper
+     * @param {Number} lineSize - line size of AxesHelper
      */
-    setRenderer() {
-        this.renderer = new THREE.WebGLRenderer();
-        this.renderer.setSize(window.innerWidth, window.innerHeight);
-        this.renderer.setClearColor ("#8bc34a", 1);
-        document.body.appendChild(this.renderer.domElement);
-    }
+    axesHelper(lineSize) {
+        this.scene.add(new THREE.AxesHelper(lineSize));        
+    }    
 
     /**
-     * Set geometry object into class property
-     * @param {Object} geometry - An Object3D
-     * @param {String} color - Hex color Codes
-     * @param {Number} metalness - How much the material is like a metal (Non-metallic materials such as wood or stone use 0.0, metallic use 1.0)
-     * @param {Number} roughness - How rough the material appears (0.0 means a smooth mirror reflection, 1.0 means fully diffuse. Default is 1.0)
-     * @returns {Object} Threejs Mesh object 
+     * Set lights position
+     * @param {Number} objectPosX - Object position in X
+     * @param {Number} objectPosY - Object position in Y
+     * @param {Number} objectPosZ - Object position in Z
      */
-    createMeshStandardMaterialObject(geometry, color, metalness, roughness, normalMap) {
-        const material = new THREE.MeshStandardMaterial({
-            normalMap: normalMap,            
-            roughness: roughness,
-            metalness: metalness,
-            color: new THREE.Color(color)
-        });
-        return new THREE.Mesh(geometry, material);
-    }
-
     setLight(objectPosX, objectPosY, objectPosZ) {
-        this.removeLights();
-        this.setLights();
+        this.removeLights([this.pointLight, this.pointLight1]);
+        this.pointLight = new THREE.PointLight(0xffffff, 0.5);
+        this.pointLight1 = new THREE.PointLight(0xffffff, 0.5);
         this.pointLight.position.set(objectPosX + 1, objectPosY + 1, objectPosZ + 1);
         this.pointLight.intensity = 1;        
         this.pointLight1.position.set(objectPosX - 1, objectPosY - 1, objectPosZ - 1);
@@ -117,14 +106,14 @@ class threeJs {
         this.scene.add(this.pointLight1);
     }
 
-    removeLights() {
-        this.scene.remove(this.pointLight);
-        this.scene.remove(this.pointLight1);
-    }
-
-    setLights() {
-        this.pointLight = new THREE.PointLight(0xffffff, 0.1);
-        this.pointLight1 = new THREE.PointLight(0xffffff, 0.1);
+    /**
+     * Remove lights from scene
+     * @param {Array} lights - Object position in X
+     */
+    removeLights(lights) {
+        lights.forEach((light) => {
+            this.scene.remove(light);
+        });
     }
 
     /**
@@ -136,98 +125,100 @@ class threeJs {
     }
 
     /**
-     * Animate - rotate geometry object
+     * run Scene - must do to render the scene and the camera
+     */
+    runScene() {
+        const renderSceneAndCam = () => {
+            this.renderer.render(this.scene, this.camera);
+            requestAnimationFrame(renderSceneAndCam);
+        }
+        renderSceneAndCam();
+    }
+
+    /**
+     * Animate object
      */
     animateObject() {
         const animate = () => {
             requestAnimationFrame(animate);
-            // this.object.rotation.x += 0.01;
-            // this.object.rotation.y += 0.01;
-            // this.line.rotation.x += 0.01;
-            // this.line.rotation.y += 0.01;
+            this.object.rotation.x += 0.01;
+            this.object.rotation.y += 0.01;
             this.object.position.y = 1;
-            this.line.position.y = 1;
-            this.renderer.render(this.scene, this.camera);
         }
         animate();
     }
-    
-    /**
-     * Remove geometry object from a scene
-     */
-    removeObjectFromScene() {
-        this.scene.remove(this.object);
-    }
 
-    /**
-     * Add geometry object to a scene
-     */
-    addObjectInScene() {
-        this.scene.add(this.object);
-    }
 
     /**
      * Remove previus geometry object and add another
      * @param {Object} geometryObject - An Threejs Object3D
      */
     async changeObjectFromScene(geometryObject) {
-        this.removeObjectFromScene();
+        this.scene.remove(this.object);
         this.setObject(geometryObject);
-        this.addObjectInScene();
+        this.scene.add(this.object);
         this.animateObject();
-    }
-
-    loadTextures() {
-        this.textureLoader = new THREE.TextureLoader();
-        this.nestTexture = this.textureLoader.load("\\textures\\nest.jpg");
-        this.stonesTexture = this.textureLoader.load("textures\\stones.jpg");
-        this.whiteWavesTexture = this.textureLoader.load("textures\\white-waves.jpg");
-        this.glassWaterDropsTexture = this.textureLoader.load("textures\\glass_water_drops.jpg");
-        this.woodTexture = this.textureLoader.load("textures\\wood.jpg");
     }
 
     /**
      * Move geometry object through the screen
      */
-    moveObject() {
+    moveObject(objects) {
         $(document).keydown((event) => {
+            if(objects?.length <= 0) {
+                return;
+            }
             switch (event.key) {
                 case "ArrowDown":
                 case "s":
                 case "S":
-                    this.object.position.z++;
-                    this.line.position.z++;
-                    this.setLight(this.object.position.x, this.object.position.y, this.object.position.z);
+                    objects.forEach((object) => {
+                        object.position.z++;
+                    });                    
                     break;
                 case "ArrowUp":
                 case "w":
                 case "W":
-                    this.object.position.z--;
-                    this.line.position.z--;
-                    this.setLight(this.object.position.x, this.object.position.y, this.object.position.z);
+                    objects.forEach((object) => {
+                        object.position.z--;
+                    });
                     break;
                 case "ArrowLeft":
                 case "a":
                 case "A":
-                    this.object.position.x--;
-                    this.line.position.x--;
-                    this.setLight(this.object.position.x, this.object.position.y, this.object.position.z);
+                    objects.forEach((object) => {
+                        object.position.x--;
+                    });
                     break;
                 case "ArrowRight":
                 case "d":
                 case "D":
-                    this.object.position.x++;
-                    this.line.position.x++;
-                    this.setLight(this.object.position.x, this.object.position.y, this.object.position.z);
+                    objects.forEach((object) => {
+                        object.position.x++;
+                    });
                     break;
                 default:
                     console.log(`event.key: ${event.key}`);
             }
-        });
+            if(objects?.length > 0) {
+                this.setLight(objects[0].position.x, objects[0].position.y, objects[0].position.z);
+            }  
+        });                      
+    }
+
+    preLoadTextures() {
+        this.texturesValues = this.create3dObjectsHelper.textures();
+        this.metallicNestMaterial = this.create3dObjectsHelper.standardMaterialValues({normalMap: this.texturesValues.nest, roughness: 0.8, metalness: 0.3});
+        this.woodMaterial = this.create3dObjectsHelper.standardMaterialValues({normalMap: this.texturesValues.wood, roughness: 0.9, metalness: 0.1});
+        this.sphereReflectMaterial = this.create3dObjectsHelper.basicMaterialValues({envMap: this.scene.background});
+    }
+
+    setDirectionalLightHelper() {
+        this.directionalLight = new THREE.DirectionalLight(0xffffff, 0.1);
+        this.dLightHelper = new THREE.DirectionalLightHelper(this.directionalLight, 5);
     }
 }
 
 $(document).ready(() => {
-    let create3dObjectsHelperInstance = new Create3dObjectsHelper(THREE);
-    let threeJsInstance = new threeJs(create3dObjectsHelperInstance);
+    let threeJsInstance = new threeJs();
 });
